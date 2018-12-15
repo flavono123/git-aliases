31e6b03a1e6623c2d58b31206e50080897a9be8f
Hansuk Hong <flavono123@gmail.com>
2018-12-15 17:15:09 +0900

    중복되는 pattern 변수로 추출하여 참조
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- rblint.rb
+++ rblint.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 cmd = 'rubocop'
-files_to_lint = `git status`.split("\n").select {|l| l.match(/new file:|modified:|새 파일:|수정함:/)}.map {|s| s.sub(/new file:|modified:|새 파일:|수정함:/, '').strip}
+interesting_pattern = /new file:|modified:|새 파일:|수정함:/
+files_to_lint = `git status`.split("\n").select {|l| l.match(interesting_pattern)}.map {|s| s.sub(interesting_pattern, '').strip}
 puts files_to_lint
 return if files_to_lint.size == 0
 system(cmd, *files_to_lint)
