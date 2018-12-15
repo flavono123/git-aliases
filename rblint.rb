16b9be6f6fc84b6a009e7ceeec317e17042f0616
Hansuk Hong <flavono123@gmail.com>
2018-12-15 17:25:58 +0900

    String#[<partten>] = '' vs. String#sub(<pattern>, '')
    []를 대체하는 것이 더 빠름:
    ```ruby
    require 'benchmark'
    Benchmark.bm do |x|
      x.report {1_000_000.times {str = '수정함:    rblint.rb'; str.sub(interesting_pattern, '')}}
      x.report {1_000_000.times {str = '수정함:    rblint.rb'; str[interesting_pattern] = ''}}
    end
           user     system      total        real
       0.435147   0.002097   0.437244 (  0.438184)
       0.326163   0.001059   0.327222 (  0.327494)
    ```
    
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- rblint.rb
+++ rblint.rb
@@ -2,7 +2,7 @@
 
 cmd = 'rubocop'
 interesting_pattern = /new file:|modified:|새 파일:|수정함:/
-files_to_lint = `git status`.split("\n").select {|l| l.match(interesting_pattern)}.map {|s| s.sub(interesting_pattern, '').strip}
+files_to_lint = `git status`.split("\n").select {|l| l.match(interesting_pattern)}.each {|s| s[interesting_pattern] = ''}.map(&:strip)
 puts files_to_lint
 return if files_to_lint.size == 0
 system(cmd, *files_to_lint)
