e332ed6d814af885d899851f167af6f9799f88eb
Hansuk Hong <flavono123@gmail.com>
2018-12-15 17:33:54 +0900

    `git status`에서 관심 있는 파일을 정제할 때 `#map` 한 번만 호출
    `#select/each/map` -> `#map`, block에서 guard 후 마지막에 `#compact`
    block argument name 명확하게 변경
    
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- rblint.rb
+++ rblint.rb
@@ -2,7 +2,11 @@
 
 cmd = 'rubocop'
 interesting_pattern = /new file:|modified:|새 파일:|수정함:/
-files_to_lint = `git status`.split("\n").select {|l| l.match(interesting_pattern)}.each {|s| s[interesting_pattern] = ''}.map(&:strip)
+files_to_lint = `git status`.split("\n").map do |line|
+  next unless l.match(interesting_pattern)
+  line[interesting_pattern] = ''
+  line.strip
+end.compact
 puts files_to_lint
 return if files_to_lint.size == 0
 system(cmd, *files_to_lint)
