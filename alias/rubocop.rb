d3abedb83c29f3e8c77fe5e7acd98510992bbffe
Hansuk Hong <flavono123@gmail.com>
2018-12-15 18:04:19 +0900

    `rblint`를 class로 정의함
    나중에 alias를 `rubocop`으로 바꿀거라 class 명을 미리 바꿈
    
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

---
+++ alias/rubocop.rb
@@ -0,0 +1,24 @@
+module Alias
+  class Rubocop
+    class << self
+      CMD = 'rubocop'
+      INTERESTING_PATTERN = /new file:|modified:|새 파일:|수정함:/
+
+      def lint
+        puts files_to_lint
+        return if files_to_lint.size == 0
+        system(CMD, *files_to_lint)
+      end
+
+      private
+
+      def files_to_lint
+        `git status`.split("\n").map do |line|
+          next unless line.match(INTERESTING_PATTERN)
+          line[INTERESTING_PATTERN] = ''
+          line.strip
+        end.compact
+      end
+    end
+  end
+end
