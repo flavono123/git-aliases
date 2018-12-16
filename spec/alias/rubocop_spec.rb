7103690fadabbc241f6453ffc241476b88cc3b1f
Hansuk Hong <flavono123@gmail.com>
2018-12-16 19:35:19 +0900

    Alias::Rubocop spec 추가
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

---
+++ spec/alias/rubocop_spec.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require_relative '../../alias/rubocop'
+
+describe Alias::Rubocop do
+  subject {Alias::Rubocop}
+
+  describe '.lint' do
+    it 'should work' do
+      subject.lint
+    end
+  end
+end
